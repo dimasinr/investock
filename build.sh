aa4a625ae4d219180ab55ad15fc2aef6b3f1b883
dimasinr <dimas.indra773@gmail.com>
2024-10-22 18:39:08 +0700

    fassfcxa

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 echo "Building project packages..."
 echo "Installing project requirements..."
 python3 -m pip install -r requirements.txt
+python3 manage.py runserver
+# echo "Migrating Database..."
+# python3 manage.py makemigrations --noinput
+# python3 manage.py migrate --noinput
 
-echo "Migrating Database..."
-python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput
-
-echo "Collecting static files..."
-python3 manage.py collectstatic --noinput
+# echo "Collecting static files..."
+# python3 manage.py collectstatic --noinput
