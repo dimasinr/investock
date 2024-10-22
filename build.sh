1a7d62b93525d6af8592dd4f3144d289d2633bad
dimasinr <dimas.indra773@gmail.com>
2024-10-22 18:28:35 +0700

    update build

--- build.sh
+++ build.sh
@@ -1,6 +1,16 @@
-echo "BUILD STARTING"
+echo "Building project packages..."
+echo "Installing project requirements..."
+python3 -m pip install -r requirements.txt
 
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py runserver --noinput --clear
+echo "Installing tailwindcss and dependencies..."
+python3 manage.py tailwind install
 
-echo "Build Clear"
+echo "Building tailwindcss..."
+python3 manage.py tailwind build
+
+echo "Migrating Database..."
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
+
+echo "Collecting static files..."
+python3 manage.py collectstatic --noinput
