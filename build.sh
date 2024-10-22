ed1596764772b5f32bc9d5e098264792bb640a08
dimasinr <dimas.indra773@gmail.com>
2024-10-22 18:38:02 +0700

    update build

--- build.sh
+++ build.sh
@@ -1,2 +1,10 @@
-pip install -r requirements.txt
-python3.9 manage.py collectstatic
+echo "Building project packages..."
+echo "Installing project requirements..."
+python3 -m pip install -r requirements.txt
+
+echo "Migrating Database..."
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
+
+echo "Collecting static files..."
+python3 manage.py collectstatic --noinput
