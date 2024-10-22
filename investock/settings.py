803aa9a06c5f1ea284b71d3e1682acfa0982af0b
dimasinr <dimas.indra773@gmail.com>
2024-10-22 18:32:45 +0700

    updaesss

--- investock/settings.py
+++ investock/settings.py
@@ -130,8 +130,8 @@ LOGIN_REDIRECT_URL = 'home'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
+# STATIC_URL = 'static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
@@ -140,6 +140,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_URL = 'static/'
-STATICFILE_DIRS = os.path.join(BASE_DIR, 'static')
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# STATICFILE_DIRS = os.path.join(BASE_DIR, 'static')
+
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles" / "static"
+STATICFILES_DIRS = [BASE_DIR / "static"]
